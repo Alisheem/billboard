eceaa97fb4f8dca5a6b047200e942ce586734998
unknown <alisheemkhan@gmail.com>
2020-07-01 20:10:11 +0530

    initial commit

---
+++ billboard_api.py
@@ -0,0 +1,32 @@
+import billboard
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_top100():
+    chart = billboard.ChartData('hot-100')
+    mydict={}
+    for i in range(0,10):
+        mydict[i] = str(chart[i])
+    return str(mydict)
+
+@app.route('/artist10')
+def get_topArtist():
+    chart = billboard.ChartData('artist-100')
+    mydict={}
+    for i in range(0,10):
+        mydict[i] = str(chart[i])
+    return str(mydict)
+
+@app.route('/ondemand')
+def get_ondemand():
+    chart = billboard.ChartData('on-demand-songs')
+    mydict={}
+    for i in range(0,10):
+        mydict[i] = str(chart[i])
+    return str(mydict)
+
+
+if __name__ == '__main__':
+    app.run()
